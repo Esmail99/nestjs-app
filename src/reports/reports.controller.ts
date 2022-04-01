82c354f14a21c7e3a0cf5344e5be1b85466ca802
Esmail Elmoussel <elmoussel12@gmail.com>
2022-04-02 00:42:49 +0200

    ..

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 import { AuthGuard } from '../guards/auth.guard';
+import { CurrentUser } from 'src/users/decorators/current-user.decorator';
+import { User } from 'src/users/user.entity';
 
 @Controller('reports')
 export class ReportsController {
@@ -9,9 +11,19 @@ export class ReportsController {
 
   @Post('/create')
   @UseGuards(AuthGuard)
-  async createReport(@Body() body: ReportDto) {
-    const createdReport = await this.reportsService.create(body);
+  async createReport(
+    @Body() body: ReportDto,
+    @CurrentUser() currentUser: User,
+  ) {
+    const createdReport = await this.reportsService.create(body, currentUser);
 
-    return createdReport;
+    const reportToBeReturned = {
+      ...createdReport,
+      userId: createdReport.user.id,
+    };
+
+    delete reportToBeReturned.user;
+
+    return reportToBeReturned;
   }
 }
